9d3aba568da37f1255df4e213e8db74e03b9efde
jad-png <j.belassiria@uca.ca.ma>
2025-12-09 07:31:51 +0100

    feat(annotations): add double annotaetion to tell app that a mehod needs to be either auth or to check user role, or both

---
+++ src/main/java/com/jyoxin/smartshop/core/annotation/HasRole.java
@@ -0,0 +1,14 @@
+package com.jyoxin.smartshop.core.annotation;
+
+import com.jyoxin.smartshop.entity.enums.Role;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HasRole {
+    Role[] value();
+}
