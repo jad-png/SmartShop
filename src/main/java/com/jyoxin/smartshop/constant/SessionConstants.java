a8b33f905d7146abadbbfdec4c4d5db810cc2560
jad-png <j.belassiria@uca.ca.ma>
2025-12-09 07:30:39 +0100

    feat(constants): add class speicific for session constants attributes would eb used for session amanagement

---
+++ src/main/java/com/jyoxin/smartshop/constant/SessionConstants.java
@@ -0,0 +1,11 @@
+package com.jyoxin.smartshop.constant;
+
+public class SessionConstants {
+    private SessionConstants() {
+        throw new UnsupportedOperationException("This is a constants class and cannot be instantiated");
+    }
+
+    public static final String USER_ID = "userId";
+    public static final String USERNAME = "username";
+    public static final String USER_ROLE = "userRole";
+}
